725a47deb92c6254881a981990680576d34217f4
ArtsimSh <techline2018@yandex.by>
2022-08-22 00:15:15 +0300

    add arifm

---
+++ pract_3.2.py
@@ -0,0 +1,6 @@
+numb1 : float = float(input('Веди первое число : '))
+numb2 : float = float(input('Веди второе число : '))
+numb3 : float = float(input('Веди третье число : '))
+arifm_sum : float = (numb1+numb2+numb3)/3
+print(round(arifm_sum, 3))
+
