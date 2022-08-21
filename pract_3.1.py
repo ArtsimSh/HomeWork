31f3371721384db1db930ae9ce8bccbcc9ba5b4c
ArtsimSh <techline2018@yandex.by>
2022-08-21 18:13:13 +0300

    add line, add print

---
+++ pract_3.1.py
@@ -0,0 +1,3 @@
+text: str = input('Введи предложение : ')
+len_world = len(text)
+print(text, sep='--')
---
+++ pract_3.3.py
@@ -0,0 +1,6 @@
+name : str = input('Введи имя : ')
+age : str = input('Введи возраст : ')
+city : str = input('Введи горо : ')
+line_f = f'Привет {name}, , твой возраст {age}, , ты из города {city}'
+print('Привет ', name, ', твой возраст ', age, ', ты из города ', city)
+print(line_f)
