9ae628851d8977e4c3e0a3c894ded8fe1ac79fd4
ArtsimSh <techline2018@yandex.by>
2022-08-21 18:42:25 +0300

    add line_f, add format, add concat

--- pract_3.3.py
+++ pract_3.3.py
@@ -1,6 +1,10 @@
 name : str = input('Введи имя : ')
 age : str = input('Введи возраст : ')
-city : str = input('Введи горо : ')
-line_f = f'Привет {name}, , твой возраст {age}, , ты из города {city}'
-print('Привет ', name, ', твой возраст ', age, ', ты из города ', city)
+city : str = input('Введи город : ')
+line_f = f'Привет {name}, твой возраст {age}, ты из города {city}'
+line_concat = 'Привет ' + name + ', твой возраст ' + age + ', ты из города ' + city
+line_format = 'Привет {}, твой возраст {}, ты из города {}'.format(name, age, city)
 print(line_f)
+print(line_format)
+print(line_concat)
+
