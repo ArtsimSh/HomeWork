58c470ff4f82218e4af8cac227a52dfddca89e01
ArtsimSh <techline2018@yandex.by>
2022-08-22 21:48:26 +0300

    add minus, add plus

---
+++ part_3.4.py
@@ -0,0 +1,6 @@
+numb1 : float = float(input('Веди первое число : '))
+numb2 : float = float(input('Веди второе число : '))
+numb3 : float = float(input('Веди третье число : '))
+plus : int = int(numb1>=1) + int(numb2>=1) + int(numb3>=1)
+minus : int = int(numb1<=-1) + int(numb2<=-1) + int(numb3<=-1)
+print(plus, minus)
