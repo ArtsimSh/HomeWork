e47eeb18ee1fae86a2aedd6e56fc9a2d7ed6b7ba
ArtsimSh <techline2018@yandex.by>
2022-08-29 15:48:17 +0300

    add palindrom or not

---
+++ pract_5.classroom.py
@@ -0,0 +1,11 @@
+stroka = input('слово : ')
+stroka = stroka.lower()
+palindrom = stroka[::-1]
+if stroka == palindrom:
+    print('Палиндром')
+    print(stroka)
+else:
+    print(('Не палиндром'))
+
+#вводится слово определить является ли оно палиндромом
+
