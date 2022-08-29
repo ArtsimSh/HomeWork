5f3cbdcbe2ee3004b1c8dc54bffb56c6f55ebc06
ArtsimSh <techline2018@yandex.by>
2022-08-29 20:57:16 +0300

    add palindrom or not, add stroka

--- pract_5.1.py
+++ pract_5.1.py
@@ -1,7 +1,11 @@
 numb_n = int(input('Введи колличество N первых чисел = '))
 numb_m = int(input('Введи M кратное = '))
 numb_k = int(input('Введи число больше K = '))
-numbers = [i for i in range(1, 100) if i % numb_m == 0 and i > numb_k]
+i = 0
+while true:
+   # numbers = [i for i in range(1, 100) if i % numb_m == 0 and i > numb_k]
+   numbers = [i if i % numb_m == 0 and i > numb_k]
+   i += 1
 print(numbers[0:numb_n])
 
 #Вывести первые N цисел кратные M и больше K
--- pract_5.classroom.py
+++ pract_5.classroom.py
@@ -1,11 +1,18 @@
-stroka = input('слово : ')
-stroka = stroka.lower()
-palindrom = stroka[::-1]
-if stroka == palindrom:
-    print('Палиндром')
-    print(stroka)
-else:
-    print(('Не палиндром'))
+#stroka = input('слово : ')
+#stroka = stroka.lower()
+#palindrom = stroka[::-1]
+#if stroka == palindrom:
+#    print('Палиндром')
+#else:
+#    print(('Не палиндром'))
 
 #вводится слово определить является ли оно палиндромом
 
+# stroka = input('Строка : ')
+# for _ in range(len(stroka)):
+#     letter = stroka[_]
+#     numb = stroka.count(letter)
+#     if numb == 1:
+#         print(letter)
+
+# вводится строка, вывести уникальные символы строки ( те которые встречаются один раз)
