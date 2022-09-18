07ac814b6e5cd9d2a7091b8f158c288d495d0395
ArtsimSh <techline2018@yandex.by>
2022-09-18 16:54:49 +0300

    create new project

---
+++ crud/__init__.py
@@ -0,0 +1,7 @@
+from typing import List
+
+from .category import CRUDCategory
+
+__all__: List[str] = [
+    'CRUDCategory'
+]
---
+++ models/anjne.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL: str = 'postgresql://rock:rock1984@localhost:5432/bh33d'
+SYNC_ENGINE = create_engine(url=DATABASE_URL)
+Session = sessionmaker(bind=SYNC_ENGINE)
+
+def create_sync_session(func):
+    def wrapper(**kwargs)
+        with Session() as session:
+            return func(**kwargs, session=session)
+    return wrapper
